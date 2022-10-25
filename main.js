753fdcdb685ce822523bc826eabe382efab98517
summydev <adegbitesumayah@gmail.com>
2022-10-25 07:37:35 +0100

    commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+ function myFunction(){
+    let list = document.querySelector(" .shownav");
+
+    if (list.style.display ==="none"){
+        list.style.display ="block"
+    }else{
+        list.style.display="none"
+    }
+ }
+
+
+ 
